b6af56a013eca915cbc050d2bcced4e567d0fa3a
Arnav Malhotra <emumalhotra@gmail.com>
2024-11-21 22:17:35 -0500

    more edits to strategy.py, ex. strat sucks, research more

--- strategy.py
+++ strategy.py
@@ -9,37 +9,42 @@ def get_tickers(tickers):
 stocks = pd.read_csv("nasdaq_screener.csv")
 
 tickerss = list(stocks["Symbol"].values)
+
 tickers = get_tickers(tickerss)
-
-random.seed(123)
-
-test_tickers = random.sample(tickers, 10)
 
 count = 0
 
 total_tickers = 0
 
-for i in tickers:
+for j in range(10):
 
-    if total_tickers % 250 == 0:
-        time.sleep(120)
+    try:
 
-    price = vbt.YFData.download(i, start="2020-11-20", end="2024-11-20").get("Close")
+        test_tickers = random.sample(tickers, 10)
 
-    ma1 = vbt.MA.run(price, 10)
-    ma2 = vbt.MA.run(price, 50)
-    ma3 = vbt.MA.run(price, 200)
-    RSI = vbt.RSI.run(price)
+        for i in test_tickers:
 
-    entries = ma1.ma_crossed_above(ma2) & RSI.rsi_above(50)# & ma2.ma_crossed_above(ma3)
-    exits = ma2.ma_crossed_above(ma1)
+            if total_tickers % 250 == 0:
+                time.sleep(120)
 
-    pf = vbt.Portfolio.from_signals(price, entries, exits, init_cash=10000)
-    if pf.total_profit() == 0:
-        print("BLANK")
-        continue
-    count += pf.total_profit()
-    print(i, pf.total_profit())
-    total_tickers += 1
+            price = vbt.YFData.download(i, start="2020-11-20", end="2024-11-20").get("Close")
+
+            ma1 = vbt.MA.run(price, 10)
+            ma2 = vbt.MA.run(price, 50)
+            ma3 = vbt.MA.run(price, 200)
+            RSI = vbt.RSI.run(price)
+
+            entries = ma1.ma_crossed_above(ma2) & RSI.rsi_above(50)# & ma2.ma_crossed_above(ma3)
+            exits = ma2.ma_crossed_above(ma1)
+
+            pf = vbt.Portfolio.from_signals(price, entries, exits, init_cash=10000)
+            if pf.total_profit() == 0:
+                print("BLANK")
+                continue
+            count += pf.total_profit()
+            print(i, pf.total_profit())
+            total_tickers += 1
+    except:
+        None
 
 print(count/total_tickers)
