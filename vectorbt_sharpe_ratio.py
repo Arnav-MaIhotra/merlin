0e4f3db390997a7add54d2a1ef80a206087b0b7e
Arnav Malhotra <emumalhotra@gmail.com>
2024-11-25 19:37:51 -0500

    edit vectorbt_sharpe_ratio.py

--- vectorbt_sharpe_ratio.py
+++ vectorbt_sharpe_ratio.py
@@ -3,6 +3,15 @@ from datetime import datetime, timedelta
 import math
 import pandas as pd
 import time
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta, MO, FR
+
+def next_business_day(start_date=None):
+    
+    next_day = start_date + timedelta(days=1)
+    while next_day.weekday() >= 5:  # 5 is Saturday, 6 is Sunday
+        next_day += timedelta(days=1)
+    return next_day
 
 def get_tickers(tickers):
     return [str(ticker) for ticker in tickers if isinstance(ticker, str) and ticker]
@@ -11,12 +20,15 @@ stocks = pd.read_csv("nasdaq_screener.csv")
 
 tickerss = list(stocks["Symbol"].values)
 
-tickers = get_tickers(tickerss)
+tickers = get_tickers(tickerss)[0:10]
 
 risk_free_rate = 0.04
 
 end_date = datetime.now() - timedelta(days=365)
 start_date = end_date - timedelta(days=365)
+
+start_date = next_business_day(start_date)
+end_date = next_business_day(end_date)
 
 sharpes = {}
 
@@ -31,6 +43,9 @@ for ticker in tickers:
     try:
 
         data = vbt.YFData.download(ticker, start=start_date, end=end_date).get("Close") #ONLY IF FULL PERIOD IS AVAILABLE
+
+        if data.index[0] != start_date.replace(hour=0, minute=0, second=0, microsecond=0):
+            print(data.index[0], start_date.replace(hour=0, minute=0, second=0, microsecond=0))
 
         daily_returns = data.pct_change().dropna()
 
