c72bc6c3a5e6a5fa2f0800251e22fc3eb7e1eabb
Arnav Malhotra <58338755+Arnav-MaIhotra@users.noreply.github.com>
2024-12-30 18:53:41 -0500

    created r & julia versions of model.py

--- current_tests.py
+++ current_tests.py
@@ -1,5 +1,23 @@
-import yfinance as yf
-import json
+import finnhub
+import os
+from dotenv import load_dotenv
 
-data = yf.download("AIMD", start="2019-09-30")
-print(data)
+load_dotenv()
+
+API_KEY = os.getenv("FINNHUB_API_KEY")
+
+finnhub_client = finnhub.Client(API_KEY)
+
+basics = finnhub_client.company_basic_financials("AAPL", "all").get("series").get("quarterly")
+
+print(basics.keys())
+
+financials = finnhub_client.financials("AAPL", "bs", "quarterly").get("financials")
+
+for i in financials:
+    print(i)
+
+reported = finnhub_client.financials_reported(symbol="AAPL", freq="quarterly").get("data")
+
+for i in reported:
+    print(i.keys())
