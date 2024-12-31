06a360f26578f118e4151b2ff88f82ee1a3c9a1d
Arnav Malhotra <58338755+Arnav-MaIhotra@users.noreply.github.com>
2024-12-30 21:05:37 -0500

    wip altman z score, test file, first 3 terms working mktcap problem

--- current_tests.py
+++ current_tests.py
@@ -8,16 +8,38 @@ API_KEY = os.getenv("FINNHUB_API_KEY")
 
 finnhub_client = finnhub.Client(API_KEY)
 
-basics = finnhub_client.company_basic_financials("AAPL", "all").get("series").get("quarterly")
+bs = finnhub_client.financials("AAPL", "bs", "quarterly").get("financials")
 
-print(basics.keys())
+ic = finnhub_client.financials("AAPL", "ic", "quarterly").get("financials")
 
-financials = finnhub_client.financials("AAPL", "bs", "quarterly").get("financials")
+cf = finnhub_client.financials("AAPL", "cf", "quarterly").get("financials")
 
-for i in financials:
+for i in ic:
+    print(i)
+
+for i in bs:
     print(i)
 
 reported = finnhub_client.financials_reported(symbol="AAPL", freq="quarterly").get("data")
 
-for i in reported:
-    print(i.keys())
+ebits = {}
+
+for i in ic:
+    date = i.get("period")
+    ebits[date] = i.get("ebit")
+
+for i in bs:
+    working_capital = i.get("currentAssets") - i.get("currentLiabilities")
+    total_assets = i.get("totalAssets")
+
+    term_1 = 1.2*working_capital/total_assets
+
+    retained_earnings = i.get("retainedEarnings")
+
+    term_2 = 1.4*retained_earnings/total_assets
+
+    date = i.get("period")
+
+    ebit = ebits[date]
+
+    term_3 = 3.3*ebit/total_assets
