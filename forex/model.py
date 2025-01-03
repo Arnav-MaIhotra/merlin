459e1aad07d9442ea09aef476c5b1a3291480541
Arnav Malhotra <58338755+Arnav-MaIhotra@users.noreply.github.com>
2025-01-02 20:08:46 -0500

    starting to use FRED and world bank apis for forex

--- forex/model.py
+++ forex/model.py
@@ -1,4 +1,7 @@
 import yfinance as yf
+import requests
+
+"""
 
 currencies = usd_currency_pairs = [
  "EUR/USD", "GBP/USD", "AUD/USD", "USD/JPY", "USD/CHF", "USD/CAD", "NZD/USD", "USD/SGD", "USD/HKD", "USD/MXN", "USD/TRY", "USD/INR", "USD/ZAR", "USD/KRW", "USD/BRL", "USD/RUB", "USD/THB", "USD/IDR", "USD/CLP", "USD/PKR", "USD/EGP", "USD/NGN", "USD/BDT", "USD/VND", "USD/AED", "USD/KWD"]
@@ -10,3 +13,5 @@ for currency in currencies:
     forex_data = yf.download(currency, end="2025-1-1")
 
     forex_data.to_csv(r"C:\Users\arnav\OneDrive\Documents\Merlin\forex\forex_data\{}.csv".format(currency.replace("/", "").replace("=X", "")))
+
+"""
